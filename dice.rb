18ab51eb3812123da16bb1f52b2de04b4c478f32
Osha Groetz <ogroetz@gmail.com>
2023-10-21 16:07:34 +0000

    layout

--- dice.rb
+++ dice.rb
@@ -27,7 +27,8 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six, { :layout => :wrapper })
+  # erb(:two_six, { :layout => :wrapper })
+  erb(:two_six)
 
 end
 
